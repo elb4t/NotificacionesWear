f437f7e6356b68e7f964ac0ca5fb110767059472
Eloy Llacer <eloy4t@gmail.com>
2017-05-28 12:32:13 +0200

    Añadir un texto largo a una notificación

--- app/src/main/java/es/elb4t/notificacioneswear/MainActivity.java
+++ app/src/main/java/es/elb4t/notificacioneswear/MainActivity.java
@@ -6,9 +6,9 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         wearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String s="Texto largo con descripción detallada de la notificación. ";
                 Intent intencionLlamar = new Intent(Intent.ACTION_DIAL,
                         Uri.parse("tel:555123456"));
                 PendingIntent intencionPendientetLlamar =
@@ -51,9 +52,10 @@ public class MainActivity extends AppCompatActivity {
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.escudo_upv))
                         .setContentTitle("Título")
                         .setContentText(Html.fromHtml("<b>Notificación</b> <u>Android<i>Wear</i></u>"))
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(s+s+s+s))
                         .setContentIntent(intencionPendienteMapa)
                         .addAction(R.mipmap.ic_action_call,  "llamar", intencionPendientetLlamar)
-                        .extend(new NotificationCompat.WearableExtender().addAction((android.support.v4.app.NotificationCompat.Action) acciones))
+                        .extend(new NotificationCompat.WearableExtender().addActions(acciones))
                         .build();
                 NotificationManagerCompat notificationManager =
                         NotificationManagerCompat.from(MainActivity.this);
